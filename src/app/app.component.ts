b3025c16c40b81ceb9992eb0aa8358482ac07880
Henry Ugochukwu <henryugochukwuagu@gmail.com>
2023-04-16 14:41:02 +0100

    done with responsive form validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,46 @@
 import { Component } from '@angular/core';
 import { User } from './user';
+import { EnrollmentService } from './enrollment.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'testApp';
-  topics = ['Angular','Vue','React'];
+  topics = ['Angular', 'Vue', 'React'];
 
   topicHasError = true;
+  submitted = false;
+  errorMessage = '';
 
+  userModel = new User(
+    '',
+    'Rob@domain.com',
+    98899899388,
+    'default',
+    'morning',
+    true
+  );
+  
+  constructor(private enrollmentService: EnrollmentService) {}
 
-  userModel = new User('','Rob@domain.com',8899899388,'default','morning',true);
-  validateTopic(value: string){
-    if(value === 'default'){
+  validateTopic(value: string) {
+    if (value === 'default') {
       this.topicHasError = true;
-    }else{
+    } else {
       this.topicHasError = false;
     }
-  }
+  };
+
+  onSubmit(userForm: any) {
+    console.log(userForm);
+    // this.submitted = true;
+    // this.enrollmentService.enroll(this.userModel)
+    // .subscribe(
+    //   data => console.log("Success!",data),
+    //   error => this.errorMessage = error.statusText
+    // )
+  };
 }
